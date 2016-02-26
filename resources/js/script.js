0a8cb4f9f54d96bd6df9ed83269e95d997c8e8d9
tripsit/user/TripRekt <triprekt@gmail.com>
2016-02-26 21:27:30 +0100

    make most resources local, resize labels that don't fit into cell (e.g. Benzodiazepines)

---
+++ resources/js/script.js
@@ -0,0 +1,16 @@
+$(function(){
+  var baseElement = $('.label').first();
+  var baseFontSize = parseFloat(baseElement.css('font-size'));
+  var baseWidth = parseInt(baseElement.css('width'));
+
+  baseFontSize = Math.round(baseFontSize);
+  baseWidth = Math.round(baseWidth * 0.92);
+
+  // maxFontPixels: Make sure labels aren't upsized to fill cell, only downsized so they don't overflow
+  // explicitWidth: Add some space so cell isn't 100% filled side-to-side
+  $('#chart .label').textfill({
+    widthOnly: true,
+    explicitWidth: baseWidth,
+    maxFontPixels: baseFontSize
+  });
+});
