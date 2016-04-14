d4ada68a0b30f9f791ef601e21cd89fd8171b301
tripsit/user/TripRekt <triprekt@gmail.com>
2016-04-14 21:56:31 +0200

    minor visual changes, icon shadow

--- resources/js/script.js
+++ resources/js/script.js
@@ -4,13 +4,17 @@ $(function(){
   var baseWidth = parseInt(baseElement.css('width'));
 
   baseFontSize = Math.round(baseFontSize);
-  baseWidth = Math.round(baseWidth * 0.92);
+  baseWidth = Math.round(baseWidth * 0.95);
 
   // maxFontPixels: Make sure labels aren't upsized to fill cell, only downsized so they don't overflow
   // explicitWidth: Add some space so cell isn't 100% filled side-to-side
   $('#chart .label').textfill({
     widthOnly: true,
-    /*explicitWidth: baseWidth,*/
+    explicitWidth: baseWidth,
     maxFontPixels: baseFontSize
   });
+
+  var SUPER_MAGIC_CONSTANT_THAT_MAKES_IT_LOOK_GOOD = 2;
+
+  $('#legend td').width($('#chart td').eq(10).width() + SUPER_MAGIC_CONSTANT_THAT_MAKES_IT_LOOK_GOOD);
 });
